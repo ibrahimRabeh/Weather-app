8ae13c1420cf702f5c23b65e86272ef4fe40ca02
ibrahimRabeh <ibrahim.rabehh@gamil.com>
2024-03-28 04:00:22 +0300

    Build project

--- src/API.js
+++
@@ -1,9 +0,0 @@
-function API(City) {
-    return fetch("https://api.weatherapi.com/v1/forecast.json?key=3fb10e08476c424d8ea115059232511&q=".concat((City), "&days=7"), {
-        method: "GET",
-        mode: "cors"
-    })
-        .then(function (response) { return response.json(); })
-        .then(function (data) { return data; });
-}
-export default API;
